ad2e80543aa12ec19e9be917c02e302054c2e364
kingnoel <66368728+kingnoel@users.noreply.github.com>
2023-01-23 14:39:40 +0100

    add book function done

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-let books = [];
+//let books = [];
 
 //add book function...
 const addBook = (ev) => {
     ev.preventDefault();
-    let book = {
-        title: document.getElementById('title').ariaValueMax,
+    let books = [{
+        title: document.getElementById('title').value,
         author: document.getElementById('author').value
-    }
-
-    books.push(book);
+    },];
 
     //clear the form for next entry...
     document.querySelector('form').reset();
 
+    const listall = document.getElementsByClassName('listall')[0];
+    console.log(listall);
     
+    books.forEach((book) => {
+        let itemHR = document.createElement('hr');
+        let buttonRemove = document.createElement('button');
+        buttonRemove.innerHTML = 'remove';
+        let itemP1 = document.createElement('p');
+        let itemP2 = document.createElement('p');
+
+        let itemP1Text = document.createTextNode(book.author);
+        let itemP2Text = document.createTextNode(book.title);
+
+        itemP1.appendChild(itemP1Text);
+        itemP2.appendChild(itemP2Text);
+
+        listall.appendChild(itemP1);
+        listall.appendChild(itemP2);
+        listall.appendChild(buttonRemove);
+        listall.appendChild(itemHR);
+    });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('btn').addEventListener('click', addBook);
+});
