cefb3fefb5520eafbb21efac2658cba4787901b6
kingnoel <66368728+kingnoel@users.noreply.github.com>
2023-01-24 16:48:04 +0100

    Added the remove feature

--- script.js
+++ script.js
@@ -1,39 +1,44 @@
-//let books = [];
-
-//add book function...
+let form = document.querySelector('form')
 const addBook = (ev) => {
     ev.preventDefault();
     let books = [{
+        id: Math.random() * 10000000,
         title: document.getElementById('title').value,
         author: document.getElementById('author').value
     },];
-
     //clear the form for next entry...
-    document.querySelector('form').reset();
-
-    const listall = document.getElementsByClassName('listall')[0];
-    console.log(listall);
-    
+    form.reset();
     books.forEach((book) => {
+        let container = document.createElement('div');
+        container.className = "book-container";
+        container.dataset.id = book.id;
         let itemHR = document.createElement('hr');
         let buttonRemove = document.createElement('button');
+        buttonRemove.className = "btn";
         buttonRemove.innerHTML = 'remove';
+        buttonRemove.dataset.id = book.id;
         let itemP1 = document.createElement('p');
         let itemP2 = document.createElement('p');
-
         let itemP1Text = document.createTextNode(book.author);
         let itemP2Text = document.createTextNode(book.title);
-
         itemP1.appendChild(itemP1Text);
         itemP2.appendChild(itemP2Text);
-
-        listall.appendChild(itemP1);
-        listall.appendChild(itemP2);
-        listall.appendChild(buttonRemove);
-        listall.appendChild(itemHR);
+        container.appendChild(itemP1);
+        container.appendChild(itemP2);
+        container.appendChild(buttonRemove);
+        container.appendChild(itemHR);
+        listall.appendChild(container);
     });
 }
-
+const listall = document.getElementsByClassName('listall')[0];
+console.log(listall);
+listall.addEventListener("click", (e) => {
+    if(e.target.classList.contains("btn")){
+        let bookId = e.target.dataset.id;
+        let bookContainer = document.querySelector(`div[data-id="${bookId}"]`);
+        bookContainer.remove();
+    }
+});
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('btn').addEventListener('click', addBook);
 });
