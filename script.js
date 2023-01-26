01cd9445fdd5fce6769bb6a3d69c77123eb07cba
kingnoel <66368728+kingnoel@users.noreply.github.com>
2023-01-26 09:41:39 +0100

    fixed linters error

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
-let form = document.querySelector('form')
-const addBook = (ev) => {
-    ev.preventDefault();
-    let books = [{
-        id: Math.random() * 10000000,
-        title: document.getElementById('title').value,
-        author: document.getElementById('author').value
-    },];
-    //clear the form for next entry...
-    form.reset();
-    books.forEach((book) => {
-        let container = document.createElement('div');
-        container.className = "book-container";
-        container.dataset.id = book.id;
-        let itemHR = document.createElement('hr');
-        let buttonRemove = document.createElement('button');
-        buttonRemove.className = "btn";
-        buttonRemove.innerHTML = 'remove';
-        buttonRemove.dataset.id = book.id;
-        let itemP1 = document.createElement('p');
-        let itemP2 = document.createElement('p');
-        let itemP1Text = document.createTextNode(book.title);
-        let itemP2Text = document.createTextNode(book.author);
-
-        itemP2.appendChild(itemP2Text);
-        itemP1.appendChild(itemP1Text);
-        container.appendChild(itemP1);
-        container.appendChild(itemP2);
-        container.appendChild(buttonRemove);
-        container.appendChild(itemHR);
-        listall.appendChild(container);
-    });
-}
+const form = document.querySelector('form');
 const listall = document.getElementsByClassName('listall')[0];
-console.log(listall);
-listall.addEventListener("click", (e) => {
-    if(e.target.classList.contains("btn")){
-        let bookId = e.target.dataset.id;
-        let bookContainer = document.querySelector(`div[data-id="${bookId}"]`);
-        bookContainer.remove();
-    }
+
+const addBook = (ev) => {
+  ev.preventDefault();
+  const books = [{
+    id: Math.random() * 10000000,
+    title: document.getElementById('title').value,
+    author: document.getElementById('author').value,
+  }];
+    // clear the form for next entry...
+  form.reset();
+  books.forEach((book) => {
+    const container = document.createElement('div');
+    container.className = 'book-container';
+    container.dataset.id = book.id;
+    const itemHR = document.createElement('hr');
+    const buttonRemove = document.createElement('button');
+    buttonRemove.className = 'btn';
+    buttonRemove.innerHTML = 'remove';
+    buttonRemove.dataset.id = book.id;
+    const itemP1 = document.createElement('p');
+    const itemP2 = document.createElement('p');
+    const itemP1Text = document.createTextNode(book.title);
+    const itemP2Text = document.createTextNode(book.author);
+
+    itemP2.appendChild(itemP2Text);
+    itemP1.appendChild(itemP1Text);
+    container.appendChild(itemP1);
+    container.appendChild(itemP2);
+    container.appendChild(buttonRemove);
+    container.appendChild(itemHR);
+    listall.appendChild(container);
+  });
+};
+// console.log(listall);
+listall.addEventListener('click', (e) => {
+  if (e.target.classList.contains('btn')) {
+    const bookId = e.target.dataset.id;
+    const bookContainer = document.querySelector(`div[data-id="${bookId}"]`);
+    bookContainer.remove();
+  }
 });
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('btn').addEventListener('click', addBook);
+  document.getElementById('btn').addEventListener('click', addBook);
 });
