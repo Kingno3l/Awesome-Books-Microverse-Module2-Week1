d0d4c31e2798f3adf9ac6dc27f69435b38e24b90
kingnoel <66368728+kingnoel@users.noreply.github.com>
2023-01-27 08:44:07 +0100

    finished stying project

--- script.js
+++ script.js
@@ -1,45 +1,53 @@
-const form = document.querySelector('form');
-const listall = document.getElementsByClassName('listall')[0];
-
+class Book {
+  constructor(id, title, author) {
+      this.id = id;
+      this.title = title;
+      this.author = author;
+  }
+  addBook() {
+      let container = document.createElement('div');
+      container.className = "book-container";
+      container.dataset.id = this.id;
+      let itemHR = document.createElement('hr');
+      let buttonRemove = document.createElement('button');
+      buttonRemove.className = "btn";
+      buttonRemove.innerHTML = 'remove';
+      buttonRemove.dataset.id = this.id;
+      let itemP1 = document.createElement('li');
+      // let itemP2 = document.createElement('li');
+      let itemP1Text = document.createTextNode(`'${this.title}'` + ' by ' + this.author);
+      // let itemP2Text = document.createTextNode(this.title);
+      itemP1.appendChild(itemP1Text);
+      // itemP2.appendChild(itemP2Text);
+      container.appendChild(itemP1);
+      // container.appendChild(itemP2);
+      container.appendChild(buttonRemove);
+      // container.appendChild(itemHR);
+      listall.appendChild(container);
+  }
+  removeBook() {
+      let bookContainer = document.querySelector(`div[data-id="${this.id}"]`);
+      bookContainer.remove();
+  }
+}
+let form = document.querySelector('form')
 const addBook = (ev) => {
   ev.preventDefault();
-  const books = [{
-    id: Math.random() * 10000000,
-    title: document.getElementById('title').value,
-    author: document.getElementById('author').value,
-  }];
-    // clear the form for next entry...
+  let id = Math.random() * 10000000;
+  let title = document.getElementById('title').value;
+  let author = document.getElementById('author').value;
+  let book = new Book(id, title, author);
+  book.addBook();
+  //clear the form for next entry...
   form.reset();
-  books.forEach((book) => {
-    const container = document.createElement('div');
-    container.className = 'book-container';
-    container.dataset.id = book.id;
-    const itemHR = document.createElement('hr');
-    const buttonRemove = document.createElement('button');
-    buttonRemove.className = 'btn';
-    buttonRemove.innerHTML = 'remove';
-    buttonRemove.dataset.id = book.id;
-    const itemP1 = document.createElement('p');
-    const itemP2 = document.createElement('p');
-    const itemP1Text = document.createTextNode(book.title);
-    const itemP2Text = document.createTextNode(book.author);
-
-    itemP2.appendChild(itemP2Text);
-    itemP1.appendChild(itemP1Text);
-    container.appendChild(itemP1);
-    container.appendChild(itemP2);
-    container.appendChild(buttonRemove);
-    container.appendChild(itemHR);
-
-    listall.appendChild(container);
-  });
-};
-// console.log(listall);
-listall.addEventListener('click', (e) => {
-  if (e.target.classList.contains('btn')) {
-    const bookId = e.target.dataset.id;
-    const bookContainer = document.querySelector(`div[data-id="${bookId}"]`);
-    bookContainer.remove();
+}
+const listall = document.getElementsByClassName('listall')[0];
+console.log(listall);
+listall.addEventListener("click", (e) => {
+  if(e.target.classList.contains("btn")){
+      let bookId = e.target.dataset.id;
+      let book = new Book(bookId);
+      book.removeBook();
   }
 });
 document.addEventListener('DOMContentLoaded', () => {
